4da4ce9de4800e1574543ae682097a79ce73b70a
javas <sivajigsivajig703@gmail.com>
2023-11-24 07:53:03 +0000

    deployfundme updated

--- solidity-foundry/Foundry-Fundme/script/DeployFundMe.s.sol
+++ solidity-foundry/Foundry-Fundme/script/DeployFundMe.s.sol
@@ -7,9 +7,12 @@ import {FundMe} from  "../src/FundMe.sol";
 
 contract DeployFundMe is Script{
 
-    function run() external {
+    function run() external returns(FundMe){
         vm.startBroadcast();
-        FundMe fundMe = new FundMe();
+        FundMe fundMe =  new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         vm.stopBroadcast();
+        return fundMe;
     }
+
+
 }
--- solidity-foundry/Foundry-Fundme/test/FundMeTest.t.sol
+++ solidity-foundry/Foundry-Fundme/test/FundMeTest.t.sol
@@ -4,13 +4,16 @@ pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from  "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
  
 contract FundMeTest is Test{
     FundMe fundMe;
 
 
     function setUp() external{
-         fundMe = new FundMe();
+        //  fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+          DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
      
     }
 
