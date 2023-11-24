a5bcaa199b3f92a7f0119997c3e49a9165266141
javas <sivajigsivajig703@gmail.com>
2023-11-24 05:49:47 +0000

    tested using alchemy node

--- solidity-foundry/Foundry-Fundme/test/FundMeTest.t.sol
+++ solidity-foundry/Foundry-Fundme/test/FundMeTest.t.sol
@@ -3,17 +3,32 @@
 pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
-
+import {FundMe} from  "../src/FundMe.sol";
+ 
 contract FundMeTest is Test{
+    FundMe fundMe;
 
-    uint number = 1;
 
     function setUp() external{
-        number = 2;
+         fundMe = new FundMe();
+     
     }
 
 
-    function testDemo() public{
-       assertEq(number, 2);
+    function testMinimusUSDIsFive() public{
+        assertEq(fundMe.MINIMUM_USD(),5e18);
+
+        // console.log(fundMe.i_owner());
+        // console.log(msg.sender);
+    
+    }
+    function testOwnerIsMsgSender() public{
+        assertEq(fundMe.i_owner(),address(this));
+    
+    }
+
+    function testPriceFeedVersionIsAccurate() public{
+        uint256 version = fundMe.getVersion();
+        assertEq(version, 4);
     }
 }
