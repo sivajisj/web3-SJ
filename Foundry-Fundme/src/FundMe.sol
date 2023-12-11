97e694b2a257e82a951b59b9e2dafe6837f95df0
sivajisj2 <sjsjsjsivaji@gmail.com>
2023-12-11 11:23:48 +0000

    refracted code

--- Foundry-Fundme/src/FundMe.sol
+++ Foundry-Fundme/src/FundMe.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.18;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
 
-error NotOwner();
+error FundMe_NotOwner();
 
 contract FundMe {
     using PriceConverter for uint256;
@@ -15,26 +15,28 @@ contract FundMe {
     // Could we make this constant?  /* hint: no! We should make it immutable! */
     address public /* immutable */ i_owner;
     uint256 public constant MINIMUM_USD = 5 * 10 ** 18;
+    AggregatorV3Interface private s_priceFeed;
     
-    constructor() {
+    constructor(address priceFeed) {
         i_owner = msg.sender;
+        s_priceFeed = AggregatorV3Interface(priceFeed);
     }
 
     function fund() public payable {
-        require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to spend more ETH!");
+        require(msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD, "You need to spend more ETH!");
         // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
         addressToAmountFunded[msg.sender] += msg.value;
         funders.push(msg.sender);
     }
     
     function getVersion() public view returns (uint256){
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        return priceFeed.version();
+      
+        return s_priceFeed.version();
     }
     
     modifier onlyOwner {
         // require(msg.sender == owner);
-        if (msg.sender != i_owner) revert NotOwner();
+        if (msg.sender != i_owner) revert FundMe_NotOwner();
         _;
     }
     
--- Foundry-Fundme/test/FundMeTest.t.sol
+++ Foundry-Fundme/test/FundMeTest.t.sol
@@ -4,12 +4,16 @@ pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol" ;
+import {DeployFundMe} from "../script/DeployFundMe.s.sol" ;
+
 
 contract FundMeTest is Test{
    FundMe fundMe;
 
   function setUp() external {
-     fundMe = new FundMe();
+    //  fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+      DeployFundMe deployFundMe = new DeployFundMe();
+      fundMe = deployFundMe.run();
   }
 
   function testMinimumDollarIsFive() public {
@@ -17,5 +21,16 @@ contract FundMeTest is Test{
     assertEq(fundMe.MINIMUM_USD(),5e18);
    
   }
+
+  function testOwnerisMsgSender() public {
+    assertEq(fundMe.i_owner(), msg.sender);
+   
+  }
+
+  function testPriceVersionIsAccurate() public {
+   uint256 version =  fundMe.getVersion();
+   assertEq(version,4);
+  }
+  // --match-test
 
 }
