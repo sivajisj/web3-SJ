e4fea7f7853e3d623ea962a9fba2fff86d69f45a
sivajisj2 <sjsjsjsivaji@gmail.com>
2023-12-12 07:42:12 +0000

    coverage improved added more test networks

--- Foundry-Fundme/test/FundMeTest.t.sol
+++ Foundry-Fundme/test/FundMeTest.t.sol
@@ -10,10 +10,15 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol" ;
 contract FundMeTest is Test{
    FundMe fundMe;
 
+    address  USER = makeAddr("user");
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant STARTING_BALANCE = 10 ether;
+
   function setUp() external {
     //  fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
       DeployFundMe deployFundMe = new DeployFundMe();
       fundMe = deployFundMe.run();
+      vm.deal(USER, STARTING_BALANCE);
   }
 
   function testMinimumDollarIsFive() public {
@@ -32,5 +37,23 @@ contract FundMeTest is Test{
    assertEq(version,4);
   }
   // --match-test
+
+   function testFundFailsWithoutEnoughEth() public {
+
+        vm.expectRevert();// Hey the nextline , should revert
+
+        fundMe.fund();// here sending 0 value
+
+        
+    }
+
+       function testFundUpdatesFundedDataStructure() public {
+
+        vm.prank(USER);
+        fundMe.fund{value : SEND_VALUE}();
+        uint256 amountFunded  = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded,SEND_VALUE);
+
+    }
 
 }
