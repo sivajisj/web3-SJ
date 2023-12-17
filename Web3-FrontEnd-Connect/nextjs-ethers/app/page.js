4541d2d2149b6d962b59a6377458b2c67f1fc60d
Sivaji gadidala <93958018+sivajisj@users.noreply.github.com>
2023-12-17 12:13:25 +0530

    Update page.js
    succesfully connected metamask to frontend using nextjs

--- Web3-FrontEnd-Connect/nextjs-ethers/app/page.js
+++ Web3-FrontEnd-Connect/nextjs-ethers/app/page.js
@@ -1,44 +1,90 @@
-"use client"
-import Image from 'next/image'
-import styles from './page.module.css'
-import { useState } from 'react'
+"use client";
+import React from 'react';
+import { Web3Provider } from '@ethersproject/providers'; // Correct import statement
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [isConnected, setIsConnected] = useState(false);
+  const [hasMetamask, setHasMetamask] = useState(false);
+  const [signer, setSigner] = useState(undefined);
 
-  const [isConnected,setIsConnected] = useState(false);
-  const [provider, setProvider] = useState()
+  useEffect(() => {
+    if (typeof window.ethereum !== "undefined") {
+      setHasMetamask(true);
+    }
+  });
 
-  async function connect(){
-       
-       if(typeof window.ethereum != 'undefined'){
+  async function connect() {
+    if (typeof window.ethereum !== 'undefined') {
+      try {
+        await ethereum.request({
+          method: 'eth_requestAccounts',
+        });
 
-           try {
-             
-            await ethereum.request({
-              method: "eth_requestAccounts"
-            });
+        setIsConnected(true);
+        let connectedProvider =new Web3Provider(window.ethereum);
+        setSigner(connectedProvider.getSigner());
+      } catch (error) {
+        setIsConnected(false);
+        console.log(error);
+      }
+    }
+  }
 
-            setIsConnected(true);
-            let connectedProvider = new ethers.providers.Web3Provider(
-              window.ethereum
-            )
-            setProvider(connectedProvider)
-             
-           } catch (error) {
-            console.log(error);
-           }
-       } 
-       
+
+  async function execute() {
+    if (typeof window.ethereum !== "undefined") {
+     const contractAddress = "0xbF0676079a18eaE5b44C69c21E1684b9EA58FadA";
+      const abi = [{
+          "inputs": [{ "internalType": "string", "name": "_name", "type": "string" },
+          { "internalType": "uint256", "name": "_favoriteNumber", "type": "uint256" }],
+          "name": "addPerson", "outputs": [],
+          "stateMutability": "nonpayable", "type": "function"
+      },
+      {
+          "inputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
+          "name": "listOfPeople", "outputs": [{ "internalType": "uint256", "name": "favoriteNumber", "type": "uint256" },
+          { "internalType": "string", "name": "name", "type": "string" }], "stateMutability": "view", "type": "function"
+      },
+      {
+          "inputs": [{ "internalType": "string", "name": "", "type": "string" }],
+          "name": "nameToFavoriteNumber", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
+          "stateMutability": "view", "type": "function"
+      }, {
+          "inputs": [], "name": "retrieve",
+          "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
+          "stateMutability": "view", "type": "function"
+      },
+      {
+          "inputs": [{ "internalType": "uint256", "name": "_favoriteNumber", "type": "uint256" }],
+          "name": "store", "outputs": [], "stateMutability": "nonpayable", "type": "function"
+      }];
+      const provider = new Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(contractAddress, abi, signer);
+      try {
+        await contract.store(42);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      console.log("Please install MetaMask");
+    }
   }
   return (
-    <main className={styles.main}>
+    <div>
+      {hasMetamask ? (
+        isConnected ? (
+          "Connected! "
+        ) : (
+          <button onClick={() => connect()}>Connect</button>
+        )
+      ) : (
+        "Please install metamask"
+      )}
 
-     
-   <button onClick={connect}>
-    connect
-   </button>
-      
-       
-    </main>
-  )
+      {isConnected ? <button onClick={() => execute()}>Execute</button> : ""}
+    </div>
+  );
 }
