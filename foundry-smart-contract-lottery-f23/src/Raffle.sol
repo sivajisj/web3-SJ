4573937f407a0a9d2d9159a53daea44138d65070
sivajisj2 <sjsjsjsivaji@gmail.com>
2023-12-18 05:31:09 +0000

    custom error is added

--- foundry-smart-contract-lottery-f23/src/Raffle.sol
+++ foundry-smart-contract-lottery-f23/src/Raffle.sol
@@ -1,3 +1,24 @@
+
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.18;
@@ -11,14 +32,19 @@ pragma solidity ^0.8.18;
 
 contract Raffle {
 
+    error Raffle_NotEnoughETHSent();
+
     uint256 private immutable i_entranceFee;
 
     constructor(uint256 entranceFee){
         i_entranceFee = entranceFee ;
     }
 
-       function enterRaffle() public {
-        
+       function enterRaffle() external payable {
+        //   require(msg.value >= i_entranceFee, "Not enough ETH to send..!");
+        if(msg.value < i_entranceFee){
+            revert Raffle_NotEnoughETHSent();
+        }
        }    
 
 
