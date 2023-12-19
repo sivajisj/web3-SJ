bdc985c2bfc672259055f4f9f8c57b9c354316e1
sivajisj2 <sjsjsjsivaji@gmail.com>
2023-12-19 08:18:13 +0000

    Network config is added

---
+++ foundry-smart-contract-lottery-f23/script/DeployRaffle.s.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script, console} from "forge-std/Script.sol";
+import {Raffle} from "../src/Raffle.sol";
+
+contract DeployRaffle is Script{
+
+    function run() external returns(Raffle) {
+        vm.startBroadcast();
+        vm.stopBroadcast();
+    }
+
+}
